2c8e03f78ee08c744c44a88bfff0123865fe0ed0
Kyohei Morihiro <kyohei.morihiro.eh@hitachi-systems.com>
2017-07-22 12:44:09 +0900

    add choice

--- messages/index.js
+++ messages/index.js
@@ -16,17 +16,40 @@ var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
 var title = '和尚の蜂蜜';
+var characters = [
+  {
+    id: 1,
+    name : 'A',
+    answer: true
+  },
+  {
+    id: 2,
+    name : 'B',
+    answer: false
+  },
+  {
+    id: 3,
+    name : 'C',
+    answer: false
+  }
+}
+var names = characters.map(character =>   character.name);
 
 bot.dialog('/', [
   function (session) {
     session.send('小話をしましょう。お題は「' + title + '」。');
     session.send('なんか小話…。');
-    builder.Prompts.choice(session, '犯人は誰でしょう？', 'A|B|C', builder.ListStyle.button);
+    builder.Prompts.choice(session, '犯人は誰でしょう？', names, builder.ListStyle.button);
   },
   function (session, results) {
     if (results.response) {
-        const answer = results.response.entity;
-        session.send('You said' + answer);
+      const answer = results.response.entity;
+      const answerCharacter = characters.filter((item, index) => item.name == answer);
+      const correctCharacter = characters.filter((item, index) => item.answer == true);
+      if (answerCharacter.answer) {
+        session.send('犯人は' + correctCharacter.name + 'です。正解！');
+      } else {
+        session.send('犯人は' + correctCharacter.name + 'です。不正解！');
     }
   }
 ]);
