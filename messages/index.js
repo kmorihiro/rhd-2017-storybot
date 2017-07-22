7d6b9cef14f6fc914010bf8dc4150c9aa7cec435
Kyohei Morihiro <kyohei.morihiro.eh@hitachi-systems.com>
2017-07-22 14:07:38 +0900

    add story

--- messages/index.js
+++ messages/index.js
@@ -15,7 +15,70 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
-var title = '和尚の蜂蜜';
+
+
+function Character(_id, _name, _target_flag, _comment_type, _ref_id) {
+  this.id = _id;
+  this.name = _name;
+  this.targetFlag = _target_flag;
+  this.commentType = _comment_type;
+  this.refId = _ref_id;
+};
+
+const stories = ["honey1","murder1","murder2"];
+const names = ["太郎", "花子", "次郎", "朋", "佑介", "恭平"];
+const commentTypes = ["target", "together"];
+const storyTitles = {
+  "honey1" : "和尚の小話",
+  "murder1" : "殺人事件１",
+  "murder2" : "殺人事件２",
+};
+
+var story;
+var charas = [];
+var names = [];
+
+
+//chara[0] = new Character(1, "太郎", true, "target", 2);
+//chara[1] = new Character(2, "花子", false, "target", 1);
+//chara[2] = new Character(3, "二郎", false, "together", 2);
+
+function initStory() {
+  const charaNum = 3;
+  const answerId = Math.floor(Math.random() * charaNum);
+  let tempNames = [].concat(names);
+
+  // set story title
+  story = stories[Math.floor(Math.random()*stories.length)];
+
+  // set characters
+  for (let i = 0, i < 3; i++) {
+    if (i === answerId) {
+      let charaIds = Array.from(Array(charaNum).keys());
+      do {
+        let target = pop.charaIds()
+      } while (target !== i)
+      charas[i] = new Character(i, tempNames.pop(Math.floor(Math.random() * tempNames.length)), true, "target", target)
+    } else {
+      let commentType = commentTypes(Math.fllor(Math.random() * commenTypes.length));
+      switch (commentType) {
+        case "target":
+          charas[i] = new Character(i, tempNames.pop(Math.floor(Math.random() * tempNames.length)), false, commentType, answerId);
+          break;
+        case "together":
+          let charaIds = Array.from(Array(charaNum).keys());
+          do {
+            let target = pop.charaIds()
+          } while (target !== i && target !== answerId)
+          charas[i] = new Character(i, tempNames.pop(Math.floor(Math.random() * tempNames.length)), false, commentType, answerId);
+          break;
+      }
+    }
+  }
+  names = charas.map(character => character.name);
+}
+
+
 var characters = [
   {
     id: 1,
@@ -33,20 +96,20 @@ var characters = [
     answer: false
   }
 ];
-var names = characters.map(character => character.name);
 
 bot.dialog('/', [
   function (session) {
-    session.send('小話をしましょう。お題は「' + title + '」。');
+    initStory();
+    session.send('小話をしましょう。お題は「' + storyTitles[story] + '」。');
     session.send('なんか小話…。');
     builder.Prompts.choice(session, '犯人は誰でしょう？', names, builder.ListStyle.button);
   },
   function (session, results) {
     if (results.response) {
       const answer = results.response.entity;
-      const answerCharacter = characters.filter((item, index) => item.name == answer);
-      const correctCharacter = characters.filter((item, index) => item.answer == true)[0];
-      if (answerCharacter[0] && answerCharacter[0].answer) {
+      const answerCharacter = charas.filter((item, index) => item.name == answer);
+      const correctCharacter = charas.filter((item, index) => item.answer == true)[0];
+      if (answerCharacter[0] && answerCharacter[0].targetFlag) {
         session.send('犯人は' + correctCharacter.name + 'です。正解！');
       } else {
         session.send('犯人は' + correctCharacter.name + 'です。不正解！');
