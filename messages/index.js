bd5c9b87a46d21e714af0c6a9e5ec9a8a3c80bbc
Kyohei Morihiro <kyohei.morihiro.eh@hitachi-systems.com>
2017-07-22 12:21:44 +0900

    Prompt test

--- messages/index.js
+++ messages/index.js
@@ -6,26 +6,38 @@ var path = require('path');
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
+  appId: process.env['MicrosoftAppId'],
+  appPassword: process.env['MicrosoftAppPassword'],
+  stateEndpoint: process.env['BotStateEndpoint'],
+  openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
-});
+title = '和尚の蜂蜜';
+
+bot.dialog('/', [
+  function (session) {
+    session.send('小話をしましょう。お題は「' + title + '」。');
+    session.send('なんか小話…。');
+    builder.Prompts.choice(session, '犯人は誰でしょう？', 'A|B|C', builder.ListStyle.button);
+  },
+  function (session, results) {
+    if (results.response) {
+        const answer = results.response.entity;
+        session.send('You said' + answer);
+    }
+  }
+]);
 
 if (useEmulator) {
-    var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(3978, function() {
-        console.log('test bot endpont at http://localhost:3978/api/messages');
-    });
-    server.post('/api/messages', connector.listen());    
+  var restify = require('restify');
+  var server = restify.createServer();
+  server.listen(3978, function() {
+    console.log('test bot endpont at http://localhost:3978/api/messages');
+  });
+  server.post('/api/messages', connector.listen());    
 } else {
-    module.exports = { default: connector.listen() }
+  module.exports = { default: connector.listen() }
 }
